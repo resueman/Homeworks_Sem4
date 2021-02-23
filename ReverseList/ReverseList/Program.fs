5b239b475842481704b687694f14f084f60f0795
resueman <44446084+resueman@users.noreply.github.com>
2021-02-23 14:38:30 +0300

    added solution and CI

---
+++ ReverseList/ReverseList/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let rec reverse list = Seq.fold (fun acc x -> x :: acc) [] list
+
+[<EntryPoint>]
+let main _ =
+    printfn "Enter items separating them with space:"
+    let items = Console.ReadLine().Split[|' '|]
+    printfn "Reversed items:"
+    reverse items |> printfn "%A"
+    0
