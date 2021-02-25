41566b11362a6a2445f5baa551f988adfe4c6d6b
resueman <44446084+resueman@users.noreply.github.com>
2021-02-26 01:08:09 +0300

    added tests

---
+++ Fibonacci/Fibonacci/Fibonacci.fs
@@ -0,0 +1,12 @@
+﻿module Fibonacci
+
+let rec fibonacci n =
+    let rec calculate previous current i = 
+        match i with
+        | i when i = n -> previous + current
+        | _ -> calculate current (previous + current) (i + 1I)            
+
+    if n = 0I then Some(0I)
+    elif n = 1I then Some(1I)
+    elif n <= 0I then None
+    else Some(calculate 0I 1I 2I)
