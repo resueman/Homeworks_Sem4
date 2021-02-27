bb6903302723bd7eeee95baa15ba9e0883ba49fc
resueman <44446084+resueman@users.noreply.github.com>
2021-02-27 14:58:57 +0300

    added functions

---
+++ EvenNumbersCounter/EvenNumbersCounter/EvenNumbersCounter.fs
@@ -0,0 +1,11 @@
+﻿namespace EvenNumbersCounter
+
+module EvenNumbersCounters =
+    let mapCounter =
+        List.map (fun x -> if x % 2 = 0 then 1 else 0) >> List.sum
+
+    let filterCounter = 
+        List.filter (fun x -> x % 2 = 0) >> List.length
+
+    let foldCounter =
+        List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc)
