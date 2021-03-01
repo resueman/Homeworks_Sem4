58429b3da8198b02aee5138fe613d9483cb7f49d
resueman <44446084+resueman@users.noreply.github.com>
2021-03-01 16:21:23 +0300

    added function

---
+++ MapForTrees/MapForTrees/Library.fs
@@ -0,0 +1,12 @@
+﻿namespace MapForTrees
+
+module MapForTrees =
+
+    type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Empty
+   
+    let rec map tree f =
+        match tree with
+        | Empty -> Empty
+        | Node (node, left, right) -> Node(f node, map left f, map right f)
