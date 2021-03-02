4989cf48b14cafa036c71cd634d0f7159754d978
resueman <44446084+resueman@users.noreply.github.com>
2021-03-02 14:01:40 +0300

    added tests

---
+++ ArithmeticParseTree/ArithmeticParseTree/Expression.fs
@@ -0,0 +1,8 @@
+﻿namespace ArithmeticParseTree
+
+type Expression = 
+    | Number of int
+    | Addition of Expression * Expression
+    | Subtraction of Expression * Expression
+    | Multiplication of Expression * Expression
+    | Division of Expression * Expression
---
+++ ArithmeticParseTree/ArithmeticParseTree/ParseTree.fs
@@ -0,0 +1,13 @@
+﻿namespace ArithmeticParseTree
+
+module ParseTree =
+    open System
+
+    let rec evaluate (n: Expression) =
+        match n with
+        | Number (value) -> value
+        | Addition (left, right) -> evaluate left + evaluate right
+        | Subtraction (left, right) -> evaluate left - evaluate right 
+        | Multiplication (left, right) -> evaluate left * evaluate right
+        | Division (_, right) when evaluate right = 0 -> raise (new DivideByZeroException("Division by zero"))
+        | Division (left, right) -> evaluate left / evaluate right
