e1c3f04a193afab0386e2b98043bcb309d742c0e
resueman <44446084+resueman@users.noreply.github.com>
2021-07-23 17:54:41 +0300

    more tests and comments added

--- WorkflowWithStrings/WorkflowWithStrings.Tests/Tests.fs
+++ WorkflowWithStrings/WorkflowWithStrings.Tests/Tests.fs
@@ -1,28 +1,36 @@
+/// Contains methods for testing the correctness of workflow,
+/// which performs calculations with numbers, given as strings
 module WorkflowWithStrings.Tests
 
 open NUnit.Framework
 open FsUnit
 open WorkflowWithStrings
 
-[<Test>]
-let ``Should return value in case of correct input`` () =
+[<TestCase(5, 8)>]
+[<TestCase(0, -5)>]
+[<TestCase(-5, -98)>]
+[<TestCase(556, 768)>]
+[<TestCase(1, -8)>]
+let ``Should return value in case of numbers given as strings`` (x: int, y: int) =
     let calculate = CalculateBuilder()
     let result = calculate {
-            let! x = "1"
-            let! y = "2"
+            let! x = x.ToString()
+            let! y = y.ToString()
             let z = x + y
             return z
         }
-    result |> should equal (Some 3)
+    result |> should equal (x + y |> Some)
 
-[<Test>]
-let ``Should return None in case of incorrect input`` () =
+[<TestCase("tyvf", "hjvk")>]
+[<TestCase("y", "m")>]
+[<TestCase("1", "x")>]
+[<TestCase("x", "1")>]
+let ``Should return None in case of incorrect input`` (x: string, y: string) =
     let calculate = CalculateBuilder()
     let result = calculate {
-        let! x = "2"
-        let! y = "m"
+        let! x = x
+        let! y = y
         let z = x + y
         return z
         }
     result |> should equal None
-
--- WorkflowWithStrings/WorkflowWithStrings/WorkflowWithStrings.fs
+++ WorkflowWithStrings/WorkflowWithStrings/WorkflowWithStrings.fs
@@ -1,12 +1,15 @@
 ﻿namespace WorkflowWithStrings
 
+/// Contains workflow performing calculations with numbers, given as strings
 module WorkflowWithStrings =
 
+    /// Active pattern for parsing string to int
     let (|Int|_|) str =
         match System.Int32.TryParse(str:string) with
         | true, int -> Some(int)
         | _ -> None
 
+    /// Workflow performing calculations with numbers, given as strings
     type CalculateBuilder() =
         member this.Bind(x, f) =
             match x with
