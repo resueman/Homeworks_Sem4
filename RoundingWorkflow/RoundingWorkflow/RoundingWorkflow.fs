86e55107f1156418aefd0da9139cee35905795c1
resueman <44446084+resueman@users.noreply.github.com>
2021-07-23 18:23:18 +0300

    tests and comments added

--- RoundingWorkflow/RoundingWorkflow/RoundingWorkflow.fs
+++ RoundingWorkflow/RoundingWorkflow/RoundingWorkflow.fs
@@ -1,11 +1,15 @@
 ﻿namespace RoundingWorkflow
 
+/// Contains workflow that performs mathematical calculations 
+/// with a specified precision (number of digits after the dot)
 module RoundingWorkflow =
     open System
 
+    /// Workflow that performs mathematical calculations 
+    /// with a specified precision (number of digits after the dot)
     type RoundingBuilder(digits: int) =
-        member this.Bind(x: float , f) = 
+        member this.Bind(x: float, f) = 
             Math.Round (x, digits) |> f
 
-        member this.Return (x:float) = 
+        member this.Return (x: float) = 
             Math.Round (x, digits) |> Some
