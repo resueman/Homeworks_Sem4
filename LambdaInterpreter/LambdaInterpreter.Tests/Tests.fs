b09bfe284c2527f64b7f5cdf5b839ccf3a0e358c
resueman <44446084+resueman@users.noreply.github.com>
2021-08-03 21:52:19 +0300

    added protection from bounding -- renaming

--- LambdaInterpreter/LambdaInterpreter.Tests/Tests.fs
+++ LambdaInterpreter/LambdaInterpreter.Tests/Tests.fs
@@ -9,21 +9,35 @@ module Tests =
     let S = Abs("x", Abs("y", Abs("z", App(App(Var "x", Var "z"), App(Var "y", Var "z")))))
     let K = Abs("x", Abs("y", Var "x"))
     let I = Abs("z", Var "z")
+    let triplet = Abs("x", App(Var "x", App(Var "x", Var "x")))
+    let w = Abs("b", App(Var "b", Var "b"))
+    let W = App(w, w)
 
     let substitutions =
         [
-            TestCaseData(App(Abs("a", Var "a"), Var "b")).Returns(Var "b")
+            // variable substitution
+            TestCaseData(App(I, Var "b")).Returns(Var "b")
             TestCaseData(App(Abs("a", Var "x"), Var "b")).Returns(Var "x")
-            TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "a")))), Var "b")).Returns(Abs("b", Abs("c", Abs("d", Var "b"))))
+            TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "a")))), Var "b")).Returns(Abs("b'", Abs("c", Abs("d", Var "b")))) //
             TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "a")))), Var "m")).Returns(Abs("b", Abs("c", Abs("d", Var "m"))))
-            TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "b")))), Var "b")).Returns(Abs("b", Abs("c", Abs("d", Var "b"))))
-            TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "c")))), Var "b")).Returns(Abs("b", Abs("c", Abs("d", Var "c"))))
+            TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "b")))), Var "b")).Returns(Abs("b'", Abs("c", Abs("d", Var "b'")))) //
+            TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "a")))), Var "c")).Returns(Abs("b", Abs("c'", Abs("d", Var "c")))) //
             TestCaseData(App(Abs("a", Abs("b", Abs("c", Abs("d", Var "x")))), Var "m")).Returns(Abs("b", Abs("c", Abs("d", Var "x"))))
             
+            // S K K
             TestCaseData(Abs("z", App(Abs("y", Var "z"), App(Abs("x", Abs("y", Var "x")), Var "z")))).Returns(I) // 4 -> 5
             TestCaseData(Abs("z", App(App(Abs("x", Abs("y", Var "x")), Var "z"), App(Abs("x", Abs("y", Var "x")), Var "z")))).Returns(I) // 3 -> 5
             TestCaseData(App(Abs("y", Abs("z", App(App(Abs("x", Abs("y", Var "x")), Var "z"), App(Var "y", Var "z")))), Abs("x", Abs("y", Var "x")))).Returns(I) // 2 -> 5
             TestCaseData(App(App(S, K), K)).Returns(I) // 1 -> 5
+
+            // If reduction is normal
+            TestCaseData(App(Abs("x", Var "y"), App(triplet, triplet))).Returns(Var "y")
+
+            // abstarction substitution
+            TestCaseData(App(I, I)).Returns(I)
+
+            // renaming
+            TestCaseData(App(App(App(Abs("x", Abs("y", Abs("z", App(App(Var "x", Var "y"), Var "z")))), App(Var "y", Var "z")), Var "x"), Var "z")).Returns(App(App(App(Var "y", Var "z"), Var "x"), Var "z"))
         ]
 
     [<Test>]
--- LambdaInterpreter/LambdaInterpreter/Library.fs
+++ LambdaInterpreter/LambdaInterpreter/Library.fs
@@ -7,8 +7,42 @@ module LambdaInterpreter =
         | Abs of string * Term
         | App of Term * Term  
 
+    type TermVars = {Free: string list; Bounded: string list}
+
     /// substitutes term to absBody instead of absVarName
     let substitute substitutedTerm absVarName absBody =
+        let getVars term = 
+            let rec get term free bounded = 
+                match term with
+                | Var name -> let freeVars = List.filter (fun x -> List.contains x bounded |> not) (name::free |> List.distinct)
+                              { Free = freeVars; Bounded = bounded }
+                | Abs (name, innerTerm) -> get innerTerm free (name::bounded |> List.distinct)
+                | App (l, r) -> let leftVars = get l free bounded
+                                let rightVars = get r free bounded
+                                let boundedVars = leftVars.Bounded @ rightVars.Bounded |> List.distinct
+                                let freeVars = leftVars.Free @ rightVars.Free 
+                                               |> List.distinct 
+                                               |> List.filter (fun x -> List.contains x boundedVars |> not)
+                                { Free = freeVars; Bounded = boundedVars }
+            get term [] []
+
+        let rename toRename term =
+            let replacements = List.map (fun x -> x + "\'") toRename |> List.zip toRename
+            let getReplacement name = List.find (fun x -> fst x = name) replacements |> snd
+            let shouldBeReplaced name = List.contains name (List.map fst replacements)
+            let rec doRenaming term = 
+                match term with
+                | Var name when shouldBeReplaced name -> getReplacement name |> Var
+                | App(l, r) -> App(doRenaming l, doRenaming r)
+                | Abs(var, innerTerm) when shouldBeReplaced var -> Abs(getReplacement var, doRenaming innerTerm)
+                | Abs(name, innerTerm) -> Abs(name, doRenaming innerTerm)
+                | _ -> term
+
+            if toRename = [] then 
+                term
+            else 
+                doRenaming term
+
         let rec replace replacedName term =
             match term with
             | Var name when name = replacedName -> substitutedTerm
@@ -16,8 +50,12 @@ module LambdaInterpreter =
             | Abs(v, t) -> Abs(v, replace replacedName t)
             | App(l, r) -> App(replace replacedName l, replace replacedName r)
 
-        replace absVarName absBody
-        
+        let absBodyVars = getVars absBody
+        let substitutedTermVars = getVars substitutedTerm
+        let renamingVars = List.filter (fun x -> List.contains x substitutedTermVars.Free) absBodyVars.Bounded
+        let renamedTerm = rename renamingVars absBody
+        replace absVarName renamedTerm
+
     let rec reduceLeft left = 
         match left with
         | App(l, r) -> 
