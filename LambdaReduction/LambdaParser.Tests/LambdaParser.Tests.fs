8d76ba538f9594610b09265e69ab4a40f24abb77
resueman <44446084+resueman@users.noreply.github.com>
2021-08-09 15:52:03 +0300

    more tests added

--- LambdaReduction/LambdaParser.Tests/LambdaParser.Tests.fs
+++ LambdaReduction/LambdaParser.Tests/LambdaParser.Tests.fs
@@ -6,12 +6,13 @@ module LambdaParser.Tests
     open AST
 
     let I = Abs("x", Var "x")
-    let XY'X = Abs("x", Abs("y", Var "x"))
+    let Ix = I
+    let Iy = Abs("y", Var "y") 
+    let K = Abs("x", Abs("y", Var "x"))
     let DZ'Z = Abs("d", Abs("z", Var "z"))
     let X = Var "x"
     let XY = App(Var "x", Var "y")
     let S = Abs("x", Abs("y", Abs("z", App(App(Var "x", Var "z"), App(Var "y", Var "z")))))
-    let K = Abs("x", Abs("y", Var "x"))
 
     [<Test>]
     [<TestCase("longName")>]
@@ -19,7 +20,7 @@ module LambdaParser.Tests
     [<TestCase("_longName")>]
     [<TestCase("_1")>]
     let ``Should support complicated names for named definitions`` (varName: string) =
-        sprintf "let %s = x\n%s" varName varName |> parseFromString |> should equal (Some X)
+        sprintf "let %s = x\n%s" varName varName |> parseFromString |> should equal <| Some X
    
     [<Test>]
     [<TestCase("longVarName")>]
@@ -31,8 +32,8 @@ module LambdaParser.Tests
     
     [<Test>]
     let ``Should parse variable correctly`` () =
-        parseFromString "let A = x\nA" |> should equal (Some X)
-        parseFromString "let A = (x)\nA" |> should equal (Some X)
+        parseFromString "let A = x\nA" |> should equal <| Some X
+        parseFromString "let A = (x)\nA" |> should equal <| Some X
 
     [<Test>]
     [<TestCase("let A = x y\nA")>]
@@ -43,7 +44,7 @@ module LambdaParser.Tests
     [<TestCase("let X = (x)\nlet Y = (y)\nX Y")>]
     [<TestCase("let X = (y)\nlet Y = (x)\nY X")>]
     let ``Should parse application of two variables correctly`` (input: string) =
-        parseFromString input |> should equal (Some XY)
+        parseFromString input |> should equal <| Some XY
 
     [<Test>]
     let ``Should parse applications with any grouping of variables correctly`` () =
@@ -55,8 +56,23 @@ module LambdaParser.Tests
         parseFromString "let A = x y c\nlet B = x y\nA B" |> should equal (Some(App(App(XY, Var "c"), XY)))
     
     [<Test>]
-    let ``Should parse application of two abstractions correctly`` () =
-        Assert.Pass()
+    [<Category("Should distinguish between abstraction and application")>]
+    [<TestCase("let A = \x.x \y.y\nA")>]
+    [<TestCase("let A = (\x.x \y.y)\nA")>]
+    [<TestCase("let A = \x.x (\y.y)\nA")>]
+    [<TestCase("let A = (\x.x (\y.y))\nA")>]
+    let ``Should parse applications two abstractions correctly`` (input: string) =
+        parseFromString input |> should equal (Some(Abs("x", App(Var "x", Iy))))
+    
+    [<Test>]
+    [<Category("Should distinguish between abstraction and application")>]
+    [<TestCase("let A = (\x.x) \y.y\nA")>]
+    [<TestCase("let A = ((\x.x) \y.y)\nA")>]
+    [<TestCase("let A = (\x.x) (\y.y)\nA")>]
+    [<TestCase("let A = ((\x.x) (\y.y))\nA")>]
+    [<TestCase("let A = \x.x\nlet B = \y.y\nA B")>]
+    let ``Should parse abstraction as part of another abstarction correctly`` (input: string) =
+        parseFromString input |> should equal <| Some(App(Ix, Iy))
 
     [<Test>]
     [<TestCase("let I = \x.x\nI")>]
@@ -64,19 +80,25 @@ module LambdaParser.Tests
     [<TestCase("let I = \x.(x)\nI")>]
     [<TestCase("let I = (\x.(x))\nI")>]
     let ``Should parse I-combinator correctly`` (input: string) =
-        parseFromString input |> should equal (Some I)
+        parseFromString input |> should equal <| Some I
 
     [<Test>]
     let ``Should parse lambda-abstraction with one argument correctly`` () =
-        Assert.Pass()
+        parseFromString "let A = \x.x y z\nA" |> should equal (Some(Abs("x", App(App(Var "x", Var "y"), Var "z"))))
+        parseFromString "let A = \x.(x y) (z)\nA" |> should equal (Some(Abs("x", App(App(Var "x", Var "y"), Var "z"))))
+        parseFromString "let A = \x.x (y z)\nA" |> should equal (Some(Abs("x", App(Var "x", App(Var "y", Var "z")))))
+        parseFromString "let A = \x.(x) y z\nA" |> should equal (Some(Abs("x", App(App(Var "x", Var "y"), Var "z"))))
+        parseFromString "let A = \x.(\y.z) k d\nA" |> should equal (Some(Abs("x", App(App(Abs("y", Var "z"), Var "k"), Var "d"))))
 
     [<Test>]
     let ``Should parse lambda-abstraction with many parameters, which returns variable, correctly`` () =
-        parseFromString "let LA = \x y.x\nLA" |> should equal (Some XY'X)
-        parseFromString "let LA = (\x y.x)\nLA" |> should equal (Some XY'X)
-        parseFromString "let LA = \x y.(x)\nLA" |> should equal (Some XY'X)
-        parseFromString "let LA = (\x y.(x))\nLA" |> should equal (Some XY'X)
-        parseFromString "let LA = \x y z k m n.x\nLA" |> should equal (Some(Abs("x", (Abs("y", (Abs("z", (Abs("k", (Abs("m", Abs("n", Var "x"))))))))))))
+        parseFromString "let LA = \x y.x\nLA" |> should equal <| Some K
+        parseFromString "let LA = (\x y.x)\nLA" |> should equal <| Some K
+        parseFromString "let LA = \x y.(x)\nLA" |> should equal <| Some K
+        parseFromString "let LA = (\x y.(x))\nLA" |> should equal <| Some K
+        parseFromString "let LA = \x y z k m n.x\nLA" |> should equal <| Some(Abs("x", (Abs("y", (Abs("z", (Abs("k", (Abs("m", Abs("n", Var "x")))))))))))
+        parseFromString "let LA = \x.\y.\z.\d.x\nLA" |> should equal (Some(Abs("x", (Abs("y", (Abs("z", (Abs("d", Var "x")))))))))
+        parseFromString "let LA = \x.(\y.(\z.(\d.x)))\nLA" |> should equal (Some(Abs("x", (Abs("y", (Abs("z", (Abs("d", Var "x")))))))))
 
     [<Test>]
     let ``Should parse difficult lambda-abstractions`` () =
@@ -92,6 +114,5 @@ module LambdaParser.Tests
         parseFromString "let LA = \x y.(x) (y)\nLA" |> should equal (Some(Abs("x", Abs("y", App(Var "x", Var "y")))))
         parseFromString "let LA = \x y z.(a b) (x y)\nLA" |> should equal (Some(Abs("x", Abs("y", Abs("z", App(App(Var "a", Var "b"), App(Var "x", Var "y")))))))
         parseFromString "let LA = \x y z.(a b)\nlet App = (x y)\nLA App" |> should equal (Some(App(Abs("x", Abs("y", Abs("z", App(Var "a", Var "b")))), XY)))
-        parseFromString "let LA1 = \x y.x\nlet LA2 = \d z.z\nLA1 LA2" |> should equal (Some(App(XY'X, DZ'Z)))
+        parseFromString "let LA1 = \x y.x\nlet LA2 = \d z.z\nLA1 LA2" |> should equal (Some(App(K, DZ'Z)))
         parseFromString "let S = \x y z.x z (y z)\nlet K = \x y.x\nS K K" |> should equal (Some(App(App(S, K), K)))
-
