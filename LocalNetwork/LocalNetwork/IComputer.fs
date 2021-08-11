c7c98dac5ed0ec3ef9bdde7db9e4d61cab75011a
resueman <44446084+resueman@users.noreply.github.com>
2021-08-11 17:55:39 +0300

    bugs fixed, tests and comments added

---
+++ LocalNetwork/LocalNetwork/IComputer.fs
@@ -0,0 +1,7 @@
+﻿namespace LocalNetwork
+
+    /// Describes computer in local network
+    type IComputer =
+        abstract member OS: IOS
+        abstract member IsInfected: bool
+        abstract member TryToGetInfected: unit -> unit
--- LocalNetwork/LocalNetwork/LocalNetwork.fs
+++ LocalNetwork/LocalNetwork/LocalNetwork.fs
@@ -1,24 +1,21 @@
-﻿module Computer
-    open System
+﻿namespace LocalNetwork
+
     open System.Collections.Generic
 
-    type IOS = 
-        abstract member ProbabilityOfInfection: float
-
-    type IComputer =
-        abstract member OS: IOS
-        abstract member IsInfected: bool
-        abstract member TryToGetInfected: unit -> unit
-
+    /// Describes local network
     type LocalNetwork(adjacencyMatrix: Dictionary<IComputer, IComputer list>) = 
 
-        let computers = Seq.toList adjacencyMatrix.Keys
+        let computers = 
+            let list1 = Dictionary.ValueCollection adjacencyMatrix |> Seq.concat |> Seq.toList
+            let list2 = Dictionary.KeyCollection adjacencyMatrix |> Seq.toList
+            (list1 @ list2) |> List.distinct
 
         let tryToInfectNeighbours computer = 
-            adjacencyMatrix.[computer] |> List.iter (fun x -> x.TryToGetInfected ())
+            if adjacencyMatrix.ContainsKey computer then
+                adjacencyMatrix.[computer] |> List.iter (fun x -> x.TryToGetInfected ())
 
-        member this.MakeStep () = 
-            computers |> List.iter tryToInfectNeighbours
+        member this.MakeStep () =
+            computers |> List.filter (fun c -> c.IsInfected) |> List.iter tryToInfectNeighbours
 
         member this.IsInTheFinalState () = 
-            computers |> List.filter (fun x -> x.OS.ProbabilityOfInfection = 0.0 || x.IsInfected) |> List.isEmpty
+            computers |> List.filter (fun x -> x.OS.ProbabilityOfInfection <> 0.0 && not x.IsInfected) |> List.isEmpty
