375a13523ca10368a129d5aa93208f0b8aae08bb
resueman <44446084+resueman@users.noreply.github.com>
2021-10-23 13:28:36 +0300

    added

---
+++ PointFree2/PointFree2.Tests/Tests.fs
@@ -0,0 +1,17 @@
+/// Contains tests for functions that maps list tail
+module PointFree2.Tests
+
+    open NUnit.Framework
+    open FsCheck
+    open MapTailFunctions
+
+    [<Test>]
+    let ``MapTail functions should be equivalent`` () =
+        Check.QuickThrowOnFailure (fun mapper list -> 
+            if List.isEmpty list then
+                true
+            else 
+                let a = f mapper list
+                let b = f' mapper list
+                let c = f'' mapper list
+                a = b && b = c)
---
+++ PointFree2/PointFree2/MapTailFunctions.fs
@@ -0,0 +1,11 @@
+﻿namespace PointFree2
+
+/// Contains equivalent functions, that take a function and a list, 
+/// and return mapped tail of given list
+module MapTailFunctions =
+    
+    let f g l = List.map g (List.tail l)
+     
+    let f' g = List.tail >> List.map g
+
+    let f'' g l = ((>>) List.tail) << List.map <| g l // let f'' = ((>>) List.tail) << List.map, без указания g и l ругается
